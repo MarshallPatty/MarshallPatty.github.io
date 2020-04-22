c74b256471920a52b86e5a8f6a3c0feb1fc4c061
MarshallPatty <marshallpatty25@gmail.com>
2020-04-22 23:32:14 +1200

    First commit

---
+++ precache-manifest.957938e8cf380fee26ff383dbcbbd6e1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8f13c2e33f1dc88b4d49342e076ee5e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "805854a43ddd331a3eea",
+    "url": "/static/js/2.6ec9de7e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.6ec9de7e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b98ed5b8b7a406f62e5",
+    "url": "/static/js/main.549c1a52.chunk.js"
+  },
+  {
+    "revision": "14369faab3182e2ac35e",
+    "url": "/static/js/runtime-main.010870a7.js"
+  }
+]);
